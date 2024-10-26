216cfe828cfaa5eac70299e772d2402b82647b16
code920309 <95957270+code920309@users.noreply.github.com>
2024-10-27 00:17:23 +0900

    웹앱 적용, 서비스워커등록, 앱로고 크기변경

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import Root from "./Root";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./service.worker.registration";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>
 );
-
+// 서비스 워커 등록
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
